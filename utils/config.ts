da16907a6018f51a4b8e97212f521f2f03c0d410
abbesezzeddine2-maker <abbesezzeddine2@gmail.com>
2026-02-02 11:06:05 +0100

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -1,5 +1,5 @@
-// C'est ici que vous configurez la connexion "Back Office"
-// Remplacez la valeur ci-dessous par l'ID de votre Google Sheet public.
-// L'ID se trouve dans l'URL de votre sheet : https://docs.google.com/spreadsheets/d/[VOTRE_ID_ICI]/edit
 
-export const GOOGLE_SHEET_ID = "1XcbyXUFHGudw_p_gDqKwUM9yreFM3sqW";
+// C'est ici que vous configurez la connexion "Back Office"
+// L'URL ci-dessous pointe vers votre Google Sheet publié au format CSV.
+
+export const GOOGLE_SHEET_URL = "https://docs.google.com/spreadsheets/d/e/2PACX-1vQCWprLVfBODf1a-ZOPuNNgsVvYKgD5NL_uIo4gQ7XOhPf8sb4HZZlpnQ1Cy-vR_A/pub?output=csv";
