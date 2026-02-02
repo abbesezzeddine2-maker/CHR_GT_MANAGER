010baabbbffd922416b1ddc8cfa445c7da900b47
abbesezzeddine2-maker <abbesezzeddine2@gmail.com>
2026-02-02 10:38:27 +0100

    Update config.ts

--- utils/config.ts
+++ utils/config.ts
@@ -2,4 +2,4 @@
 // Remplacez la valeur ci-dessous par l'ID de votre Google Sheet public.
 // L'ID se trouve dans l'URL de votre sheet : https://docs.google.com/spreadsheets/d/[VOTRE_ID_ICI]/edit
 
-export const GOOGLE_SHEET_ID = "1XcbyXUFHGudw_p_gDqKwUM9yreFM3sqW";
+export const GOOGLE_SHEET_ID = "2PACX-1vQCWprLVfBODf1a-ZOPuNNgsVvYKgD5NL_uIo4gQ7XOhPf8sb4HZZlpnQ1Cy-vR_A";
